2add646c9f3cfe22f264169379cf40e53d591309
jb <jb@levillageducode.fr>
2022-01-27 14:49:29 +0100

    Set base for eleventy

---
+++ .eleventy.js
@@ -0,0 +1,14 @@
+/* Config file */
+module.exports = function(eleventyConfig) {
+
+    // Ce dossier est cloné lors du build
+    eleventyConfig.addPassthroughCopy("./src/img");
+    eleventyConfig.addPassthroughCopy("./src/css");
+
+    return {
+      dir: {
+          input: "src",
+          output: "dist"
+      }
+    }
+};
